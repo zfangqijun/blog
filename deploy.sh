7f5ea1f8df3eb2e9c8bb8dd6d5d2c0385d769cb8
zfangqijun <zfangqj@gmail.com>
2020-03-24 15:09:25 +0800

    feat: deploy sh done

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+echo "压缩dist"
 zip -r dist dist/
 
-scp ./dist.zip root@172.81.246.118://usr/share/nginx/html/ 
-unzip -o dist.zip 
+echo "上传dist.zip"
+scp ./dist.zip root@172.81.246.118:/usr/share/nginx/html/ 
+
+echo "连接服务器，解压"
+ssh root@172.81.246.118 "cd /usr/share/nginx/html/ ; unzip -o dist.zip"
+
+echo "删除本地dist.zip"
+rm dist.zip
