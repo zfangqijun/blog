7b9f8d8eac6da6bffcefb0861135b40caf0865ee
zfangqijun <zfangqj@gmail.com>
2020-03-26 14:40:13 +0800

    fix: sort order

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ const sortByDate = (files) => {
     files.sort((a, b) => {
         const date1 = new Date(a.opt.date);
         const date2 = new Date(b.opt.date);
-        return date1.getTime() - date2.getTime();
+        return date2.getTime() - date1.getTime();
     })
     return files;
 }
